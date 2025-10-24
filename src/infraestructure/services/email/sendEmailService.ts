7f21ce297d2833dfaab0d12e91d9b89f6da72297
Yasmany Boffill Espinosa <yasmani.boffill@tamerdigital.com>
2025-10-24 02:36:49 -0400

    fix: nueva

--- src/infraestructure/services/email/sendEmailService.ts
+++
@@ -1,5 +0,0 @@
-import { SmtpEmailRepository } from '@/infraestructure/repositories';
-import { SendEmailUseCase } from '@/application/use-cases';
-
-const emailRepository = new SmtpEmailRepository();
-export const sendEmail = new SendEmailUseCase(emailRepository);
