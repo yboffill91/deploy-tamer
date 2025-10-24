561b9660423feda77ad480026c9d987aad0af7b2
Yasmany Boffill Espinosa <yasmani.boffill@tamerdigital.com>
2025-10-24 02:28:47 -0400

    'ft: rdp

--- src/application/use-cases/SendOnboardingEmailUseCase.ts
+++ src/application/use-cases/SendOnboardingEmailUseCase.ts
@@ -1,15 +1,15 @@
-import { SendOnboardingEmailDTO } from '@/application/dto';
-import { EmailServiceRepository } from '@/core';
-import { EmailFactory } from '../services/EmailFactory';
+// import { SendOnboardingEmailDTO } from '@/application/dto';
+// import { EmailServiceRepository } from '@/core';
+// import { EmailFactory } from '../services/EmailFactory';
 
-export class SendOnboardingEmailUseCase {
-  constructor(
-    private readonly emailRepository: EmailServiceRepository,
-    private readonly emailFactory: EmailFactory
-  ) {}
+// export class SendOnboardingEmailUseCase {
+//   constructor(
+//     private readonly emailRepository: EmailServiceRepository,
+//     private readonly emailFactory: EmailFactory
+//   ) {}
 
-  async execute({ to, name }: SendOnboardingEmailDTO): Promise<void> {
-    const email = this.emailFactory.createWelcomeEmail(to, name);
-    await this.emailRepository.send(email);
-  }
-}
+//   async execute({ to, name }: SendOnboardingEmailDTO): Promise<void> {
+//     const email = this.emailFactory.createWelcomeEmail(to, name);
+//     await this.emailRepository.send(email);
+//   }
+// }
