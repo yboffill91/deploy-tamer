0cdfff8746d04cf95081170866254ac8efad36ba
Yasmany Boffill Espinosa <yasmani.boffill@tamerdigital.com>
2025-10-31 09:30:55 -0400

    new: deploy

--- src/lib/mocks/Users.Mock.ts
+++ src/lib/mocks/Users.Mock.ts
@@ -1,12 +1,12 @@
-import { Email, Role, User } from '@/core';
+import { Email, User } from '@/core';
 
 export const mockUsers = [
-  new User('1', new Email('user1@mail.com'), 'User One', '', new Role('user')),
+  new User('1', new Email('user1@mail.com'), 'User One', ''),
   new User(
     '2',
     new Email('customer1@mail.com'),
     'Customer One',
-    '',
-    new Role('customer')
+    ''
+    // new Role('customer')
   ),
 ];
