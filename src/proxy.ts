0033d4a24a5485d1d70dd94189e8ac4b5827acba
Yasmany Boffill Espinosa <yasmani.boffill@tamerdigital.com>
2025-10-31 21:27:43 -0400

    redeply: cambos en el middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const protectedRoutes = ['/admin', '/verify_account'];
+const protectedRoutes = ['/admin'];
 
 export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
