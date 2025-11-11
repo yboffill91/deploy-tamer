4148c5326f6b48928004a91491e91bb7bf5b1c90
Yasmany Boffill Espinosa <yasmani.boffill@tamerdigital.com>
2025-11-11 17:24:12 -0500

    cambios toasts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,59 @@
-import { bodyFont } from "@/lib/fonts";
-import { cn } from "@/lib/utils";
-import { ReactNode } from "react";
-import "./globals.css";
-import { Toaster } from "react-hot-toast";
-import { AuthProvider } from "@/modules/auth";
-import { ThemeProvider } from "@/providers/ThemeProvider";
+import { bodyFont } from '@/lib/fonts';
+import { cn } from '@/lib/utils';
+import { ReactNode } from 'react';
+import './globals.css';
+import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from '@/modules/auth';
+import { ThemeProvider } from '@/providers/ThemeProvider';
+import { success } from 'zod';
 
 const layout = ({ children }: { children: Readonly<ReactNode> }) => {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={cn(
-          "antialiased bg-background text-foreground transition-all duration-300",
+          'antialiased bg-background text-foreground transition-all duration-300',
           bodyFont.className
         )}
       >
         <AuthProvider>
           <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
+            attribute='class'
+            defaultTheme='system'
             enableSystem
             disableTransitionOnChange={false}
           >
             {children}
-            <Toaster position="top-center" />
+            <Toaster
+              position='top-center'
+              toastOptions={{
+                className: '',
+                style: {
+                  background: '#FFFFFF',
+                  border: '1px solid #CCCCCC',
+                  padding: '16px',
+                  color: '#000000',
+                },
+                success: {
+                  style: {
+                    background: '#ABD9AB',
+                    color: '#204620',
+                  },
+                },
+                error: {
+                  style: {
+                    background: '#F4CDCD',
+                    color: '#641616',
+                  },
+                },
+                loading: {
+                  style: {
+                    background: '#ECEFF1',
+                    color: '#000000',
+                  },
+                },
+              }}
+            />
           </ThemeProvider>
         </AuthProvider>
       </body>
